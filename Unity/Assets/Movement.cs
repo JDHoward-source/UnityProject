69b2c077befed4fbb7e232a7de625afd9b3dfff9
John Howard <john@westpierstudio.com>
2019-08-19 13:56:22 +0100

    Initial Scene Setup

---
+++ Unity/Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector3 velocity;
+    Vector3 position;
+    float friction;
+    float acceleration;
+    void Update()
+    {
+        velocity += GetInput()*acceleration * Time.deltaTime;
+        position += velocity * Time.deltaTime;
+        velocity -= friction * Time.deltaTime * velocity;
+    }
+
+    Vector3 GetInput()
+    {
+        return  new Vector3( Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")).normalized;
+    }
+}
