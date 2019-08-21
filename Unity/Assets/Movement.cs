b2a318c4ff4ca154543ce2021a4a90772423baa2
John Howard <john@westpierstudio.com>
2019-08-21 13:57:52 +0100

    Explortation into rb.MovePosition

--- Unity/Assets/Movement.cs
+++ Unity/Assets/Movement.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
     Vector3 velocity;
     Vector3 position;
     float friction;
-    float acceleration;
+    float acceleration = 1;
     void Update()
     {
-        velocity += GetInput()*acceleration * Time.deltaTime;
+        velocity += GetInput() * 0.5f * Time.deltaTime;
         position += velocity * Time.deltaTime;
-        velocity -= friction * Time.deltaTime * velocity;
+        // velocity -= friction * Time.deltaTime * velocity;
+        rb.MovePosition(transform.position + position);
     }
 
     Vector3 GetInput()
     {
         return  new Vector3( Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")).normalized;
     }
+    // private void Update()
+    // {
+    //     DoMovement();
+    // }
+    // private void DoMovement()
+    // {
+    //     var inputvector = new Vector3( Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+    //     Vector3 direction = (inputvector - transform.position).normalized;
+    //     rigidbody.MovePosition(transform.position + inputvector * 5f * Time.deltaTime);
+     
+    // }
 }
