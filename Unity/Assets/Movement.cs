0f33a3555368813b84637fbc40cb1c55756c4cb0
John Howard <john@westpierstudio.com>
2019-08-22 13:55:45 +0100

    Movement Experimentation

--- Unity/Assets/Movement.cs
+++ Unity/Assets/Movement.cs
@@ -7,20 +7,43 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private Rigidbody rb;
     Vector3 velocity;
-    Vector3 position;
+    Vector3 direction;
     float friction;
     float acceleration = 1;
+    float targetSpeed = 10;
+    float maxSpeed = 12;
+
+    float Horizontal;
+    float vertical;
     void Update()
     {
-        velocity += GetInput() * 0.5f * Time.deltaTime;
-        position += velocity * Time.deltaTime;
-        // velocity -= friction * Time.deltaTime * velocity;
-        rb.MovePosition(transform.position + position);
+        direction = GetInput();
+        // Vector3 targetVelocity = transform.rotation * direction * targetSpeed;
+        // if(targetVelocity.magnitude > maxSpeed)
+        // {
+        //     targetVelocity
+        // }
+
+
+        velocity = direction * acceleration * Time.deltaTime;
+        velocity -= friction * Time.deltaTime * velocity;
+
+        MovementCalc(velocity);
+
+        
+    }
+    void FixedUpdate(){
+    }
+    void MovementCalc(Vector3 velocity)
+    {
+        rb.MovePosition(transform.position + velocity);
     }
 
     Vector3 GetInput()
     {
-        return  new Vector3( Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical")).normalized;
+        Horizontal = Input.GetAxis ("Horizontal");
+        vertical = Input.GetAxis ("Vertical");
+        return  new Vector3( Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized;
     }
     // private void Update()
     // {
