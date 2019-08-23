dc8f184bc2ebab41f0bc2a9058bb83124b7d0dc3
John Howard <john@westpierstudio.com>
2019-08-23 14:07:52 +0100

    Velocity Investigation

--- Unity/Assets/Movement.cs
+++ Unity/Assets/Movement.cs
@@ -9,28 +9,33 @@ public class Movement : MonoBehaviour
     Vector3 velocity;
     Vector3 direction;
     float friction;
-    float acceleration = 1;
-    float targetSpeed = 10;
+    float acceleration = 1.5f;
+    float speed = 5;
     float maxSpeed = 12;
 
     float Horizontal;
     float vertical;
+
+    float targetVelocity = 2f;
+    float maxVelocity = 5f;
+
+    
     void Update()
     {
         direction = GetInput();
-        // Vector3 targetVelocity = transform.rotation * direction * targetSpeed;
-        // if(targetVelocity.magnitude > maxSpeed)
+
+         acceleration = (direction.normalized * GetRequiredAcceleraton(targetVelocity, 1)).sqrMagnitude;
+
+        velocity = direction * (velocity.sqrMagnitude + (acceleration*Time.deltaTime));
+        
+        // Vector3 v;
+        // if(velocity.sqrMagnitude > maxVelocity)
         // {
-        //     targetVelocity
+        //     v = velocity.normalized * maxVelocity;
+        //     velocity = v;
         // }
 
-
-        velocity = direction * acceleration * Time.deltaTime;
-        velocity -= friction * Time.deltaTime * velocity;
-
         MovementCalc(velocity);
-
-        
     }
     void FixedUpdate(){
     }
@@ -39,21 +44,22 @@ public class Movement : MonoBehaviour
         rb.MovePosition(transform.position + velocity);
     }
 
+     float GetRequiredAcceleraton(float aFinalSpeed, float aDrag)
+    {
+        return GetRequiredVelocityChange(aFinalSpeed, aDrag) / Time.fixedDeltaTime;
+    }
+
+     float GetRequiredVelocityChange(float aFinalSpeed, float aDrag)
+    {
+        float m = Mathf.Clamp01(aDrag * Time.fixedDeltaTime);
+        return aFinalSpeed * m / (1 - m);
+    }
     Vector3 GetInput()
     {
         Horizontal = Input.GetAxis ("Horizontal");
         vertical = Input.GetAxis ("Vertical");
         return  new Vector3( Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical")).normalized;
     }
-    // private void Update()
-    // {
-    //     DoMovement();
-    // }
-    // private void DoMovement()
-    // {
-    //     var inputvector = new Vector3( Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-    //     Vector3 direction = (inputvector - transform.position).normalized;
-    //     rigidbody.MovePosition(transform.position + inputvector * 5f * Time.deltaTime);
-     
-    // }
+
+
 }
