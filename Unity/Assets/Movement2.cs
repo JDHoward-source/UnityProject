11a25f65ddc48ffb72f4aa0bdad188803b91c7a5
John Howard <john@westpierstudio.com>
2019-08-28 08:40:02 +0100

    Additional movement method.
    Use of lerping.

---
+++ Unity/Assets/Movement2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2 : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+    [SerializeField]
+    private float maxSpeed;
+
+    [SerializeField]
+    private float friction;
+
+    [SerializeField]
+    private float inputDamping;
+
+    private Vector3 idealVelocity;
+    private Vector3 currentVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {  
+        idealVelocity = Vector3.Lerp(idealVelocity, GetInput() * maxSpeed, inputDamping * Time.deltaTime);
+        currentVelocity = Vector3.MoveTowards(currentVelocity, idealVelocity, friction * Time.time);
+        transform.position += currentVelocity * Time.deltaTime;
+        //rb.MovePosition(transform.position + (currentVelocity * Time.deltaTime)); // making work with rb instead of transform
+
+        Debug.DrawRay(transform.position, idealVelocity, Color.red);
+        Debug.DrawRay(transform.position, currentVelocity, Color.green);
+    }
+
+    private Vector3 GetInput()
+    {
+        return new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+    }
+}
